10aa1ecbc7f5e7cc963037cc279694fd553eb284
Alberto Sanchez Hernandez <alberto.sanchez@mail.com>
2015-06-18 19:50:20 -0500

    adding more parameters

--- python/Onia2MuMuRootupler_cfi.py
+++ python/Onia2MuMuRootupler_cfi.py
@@ -3,5 +3,8 @@ import FWCore.ParameterSet.Config as cms
 rootuple = cms.EDAnalyzer('Onia2MuMuRootupler',
                           dimuons = cms.InputTag("onia2MuMuPAT"),
                           primaryVertices = cms.InputTag("offlinePrimaryVertices"),
-                          isMC = cms.bool(True)
+                          onia_pdgid = cms.uint32(443),
+                          onia_mass = cms.vdouble(2.2,4.0),
+                          isMC = cms.bool(True),
+                          OnlyBest = cms.bool(True)
                           )
