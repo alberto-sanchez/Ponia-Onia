6dd1db4c345eb0efacac6c078b9f7f63ee36cce3
Alberto Sanchez Hernandez <alberto.sanchez@mail.com>
2015-09-16 21:18:33 -0500

    making varibles names similar to those in onia2mumu rootupler

--- src/OniaMM.cc
+++ src/OniaMM.cc
@@ -51,13 +51,18 @@ private:
   virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
   const reco::Candidate* GetStableParticle(const reco::Candidate*);
   const reco::Candidate* GetAncestor(const reco::Candidate*);
+
+  int  pdgid_;
   
   // Variables
-  TLorentzVector muonP_p4;
-  TLorentzVector muonM_p4;
-  TLorentzVector dimuon_p4;
-  Int_t onia_id, mom_id;
-  TTree* gen_tree;
+  UInt_t    run;
+  ULong64_t event;
+  UInt_t    lumiblock;
+  TLorentzVector gen_muonP_p4;
+  TLorentzVector gen_muonM_p4;
+  TLorentzVector gen_dimuon_p4;
+  Int_t mother_pdgId,dimuon_pdgId;
+  TTree* onia_tree;
   
 };
 
@@ -65,14 +70,22 @@ private:
 // constructors and destructor
 //
 
-OniaMM::OniaMM(const edm::ParameterSet& iConfig) {
-	edm::Service<TFileService> fs;
-	gen_tree = fs->make<TTree>("GenTree","Tree of genParticles");
-        gen_tree->Branch("mother_pdgId",  &mom_id,          "mom_id/I");
-        gen_tree->Branch("dimuon_pdgId",  &onia_id,         "onia_id/I");
-	gen_tree->Branch("gen_muonP_p4",  "TLorentzVector", &muonP_p4);
-	gen_tree->Branch("gen_muonM_p4",  "TLorentzVector", &muonM_p4);
-        gen_tree->Branch("gen_dimuon_p4", "TLorentzVector", &dimuon_p4);
+OniaMM::OniaMM(const edm::ParameterSet& iConfig):
+pdgid_(iConfig.getParameter<uint32_t>("onia_pdgid"))
+{
+
+  edm::Service < TFileService > fs;
+  onia_tree = fs->make < TTree > ("oniaTree", "Tree of Onia2MuMu");
+  onia_tree->Branch("run",      &run,      "run/i");
+  onia_tree->Branch("event",    &event,    "event/l");
+  onia_tree->Branch("lumiblock",&lumiblock,"lumiblock/i");
+
+  std::cout << "Onia2MuMuRootupler::Onia2MuMuRootupler: Onia id " << pdgid_ << std::endl;
+  onia_tree->Branch("mother_pdgId",  &mother_pdgId,     "mother_pdgId/I");
+  onia_tree->Branch("dimuon_pdgId",  &dimuon_pdgId,     "dimuon_pdgId/I");
+  onia_tree->Branch("gen_dimuon_p4", "TLorentzVector",  &gen_dimuon_p4);
+  onia_tree->Branch("gen_muonP_p4",  "TLorentzVector",  &gen_muonP_p4);
+  onia_tree->Branch("gen_muonN_p4",  "TLorentzVector",  &gen_muonM_p4);
 }
 
 OniaMM::~OniaMM() {}
@@ -109,25 +122,31 @@ void OniaMM::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
 
   edm::Handle<reco::GenParticleCollection> GenParticles;
   iEvent.getByLabel("genParticles",GenParticles);
-  
-  onia_id = 0;
-  mom_id = 0;
+
+  run       = iEvent.id().run();
+  event     = iEvent.id().event();
+  lumiblock = iEvent.id().luminosityBlock();
+
+  dimuon_pdgId = 0;
+  mother_pdgId = 0;
+
+  gen_dimuon_p4.SetPtEtaPhiM(0.,0.,0.,0.);
+  gen_muonP_p4.SetPtEtaPhiM(0.,0.,0.,0.);
+  gen_muonM_p4.SetPtEtaPhiM(0.,0.,0.,0.);
 
   if ( GenParticles.isValid() ) {
 
      for ( reco::GenParticleCollection::const_iterator itParticle = GenParticles->begin(); itParticle != GenParticles->end(); ++itParticle ) {
          Int_t pdgId = itParticle->pdgId();
-         onia_id = 0;
+         dimuon_pdgId = 0;
          int foundit = 0;
-         if ( (pdgId == 443 || pdgId == 100443 || pdgId == 553 || pdgId == 100553 || pdgId == 200553)
-              && itParticle->status() == 2
-            ) { 
+         if ( (abs(pdgId) == pdgid_) && (itParticle->status() == 2) ) {
             int n = itParticle->numberOfDaughters();
             if (n < 2) continue;
 
             const  reco::Candidate* onia = &(*itParticle);
             foundit++;
-            onia_id = pdgId;
+            dimuon_pdgId = pdgId;
 
             bool yetM = false;
             bool yetP = false;
@@ -137,33 +156,33 @@ void OniaMM::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
                 Int_t dauId = d->pdgId();
                 if ( dauId == 13 && !yetM) {
                    const  reco::Candidate* mM = GetStableParticle(d);
-                   muonM_p4.SetPtEtaPhiM(mM->pt(),mM->eta(),mM->phi(),mM->mass());
+                   gen_muonM_p4.SetPtEtaPhiM(mM->pt(),mM->eta(),mM->phi(),mM->mass());
                    foundit++;
                    yetM = true;
                 } 
                 if ( dauId == -13 && !yetP) {
                    const  reco::Candidate* mP = GetStableParticle(d);
-                   muonP_p4.SetPtEtaPhiM(mP->pt(),mP->eta(),mP->phi(),mP->mass());
+                   gen_muonP_p4.SetPtEtaPhiM(mP->pt(),mP->eta(),mP->phi(),mP->mass());
                    foundit++;
                    yetP = true;
                 } 
             }
             if ( foundit == 3 ) {
-               mom_id = GetAncestor(onia)->pdgId();
-               dimuon_p4 = muonM_p4 + muonP_p4;
+               mother_pdgId = GetAncestor(onia)->pdgId();
+               gen_dimuon_p4 = gen_muonM_p4 + gen_muonP_p4;
                break;
             } else {
                foundit = 0;
-               onia_id = 0;
-               mom_id = 0;
+               dimuon_pdgId = 0;
+               mother_pdgId = 0;
             }
          }  // if ( pdg
 
       }   // for ( reco
   }     // if (GenPar
   // sanity check
-  if ( ! onia_id ) std::cout << "OniaMM: Decay not found [" << iEvent.id().run() << "," << iEvent.id().event() << "]" << std::endl; 
-  else gen_tree ->Fill();
+  if ( ! dimuon_pdgId ) std::cout << "OniaMM: Decay not found [" << iEvent.id().run() << "," << iEvent.id().event() << "]" << std::endl; 
+  else onia_tree ->Fill();
 }
 
 // ------------ method called once each job just before starting event loop  ------------
