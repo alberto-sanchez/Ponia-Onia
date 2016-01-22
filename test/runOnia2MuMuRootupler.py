42b69e7d6348b8228cb5df59c1cd75b842d77475
Alberto Sanchez Hernandez <alberto.sanchez@mail.com>
2016-01-22 08:17:05 -0600

    replacing incorrect config

--- test/runOnia2MuMuRootupler.py
+++ test/runOnia2MuMuRootupler.py
@@ -2,15 +2,8 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("Rootuple")
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
-process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-process.load('Configuration.StandardSequences.MagneticField_38T_cff')
-process.load('Configuration.StandardSequences.Reconstruction_cff')
-
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
-from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data', '')
-
 process.MessageLogger.cerr.FwkReport.reportEvery = 500
+
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 
 process.source = cms.Source("PoolSource",
