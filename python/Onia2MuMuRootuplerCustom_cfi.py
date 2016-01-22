63fb18f6ae8d5f4fe30db9abf0802421f7d42b69
Alberto Sanchez Hernandez <alberto.sanchez@mail.com>
2016-01-22 08:09:17 -0600

    the corresponding configuration

---
+++ python/Onia2MuMuRootuplerCustom_cfi.py
@@ -0,0 +1,29 @@
+import FWCore.ParameterSet.Config as cms
+
+rootuple = cms.EDAnalyzer('Onia2MuMuRootuplerCustom',
+                          dimuons = cms.InputTag("onia2MuMuPAT"),
+                          muons = cms.InputTag("replaceme"),
+                          primaryVertices = cms.InputTag("offlinePrimaryVertices"),
+                          TriggerResults = cms.InputTag("TriggerResults", "", "HLT"),
+                          onia_pdgid = cms.uint32(443),
+                          onia_mass_cuts = cms.vdouble(2.9,3.3),
+                          isMC = cms.bool(True),
+                          OnlyBest = cms.bool(True),
+                          OnlyGen = cms.bool(False),
+                          HLTLastFilters = cms.vstring('hltDisplacedmumuFilterDimuon10JpsiBarrel',
+                                                       'hltDisplacedmumuFilterDimuon16Jpsi',
+                                                       'hltDisplacedmumuFilterDimuon20Jpsi'
+                          ),
+                          propagatorStation1 = cms.PSet(
+                                                        useStation2 = cms.bool(False),
+                                                        useTrack = cms.string("tracker"),
+                                                        useState = cms.string("atVertex"),  # for AOD
+                                                        useSimpleGeometry = cms.bool(True),
+                          ),
+                          propagatorStation2 = cms.PSet(
+                                                        useStation2 = cms.bool(True),
+                                                        useTrack = cms.string("tracker"),
+                                                        useState = cms.string("atVertex"),  # for AOD
+                                                        useSimpleGeometry = cms.bool(True),
+                          )                         
+)
