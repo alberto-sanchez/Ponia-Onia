84253060fb43bff0b884a8da5e5c30a48a59e7fa
Alberto Sanchez Hernandez <alberto.sanchez@mail.com>
2016-05-19 18:42:47 +0200

    adding example config for Onia2MuMuRootuplerCustom

---
+++ test/runOnia2MuMuRootuplerCustom.py
@@ -0,0 +1,35 @@
+import FWCore.ParameterSet.Config as cms
+process = cms.Process("Rootuple")
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data', '')
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:inputfile.root')
+)
+
+process.TFileService = cms.Service("TFileService",
+        fileName = cms.string('file:runOnia2MuMuRootupler.root'),
+)
+
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True))
+
+process.load('Ponia.Onia.Onia2MuMuRootuplerCustom_cfi')
+process.p = cms.Path(process.rootuple)
+process.rootuple.onia_mass_cuts = cms.vdouble(2.9,3.3)
+process.rootuple.isMC = cms.bool(False)
+process.rootuple.HLTLastFilters = cms.vstring(
+	'hltDisplacedmumuFilterDimuon10JpsiBarrel',
+	'hltDisplacedmumuFilterDimuon16Jpsi',
+	'hltDisplacedmumuFilterDimuon20Jpsi'
+	)
